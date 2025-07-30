b74ca05ba64069db4e7672dfe00c27fdfaffb705
mxkxx <mxkxx@naver.com>
2025-07-30 13:50:48 +0900

    [#3] chore: 개발환경에서만 확인하는 로그 유틸함수

---
+++ shared/util/dev-log.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { AxiosError, type AxiosRequestConfig, type AxiosResponse } from 'axios';
+
+import { isDevelop } from './is-develop';
+
+/* eslint-disable no-console */
+export const log = {
+  common(message: string, data?: any) {
+    if (isDevelop()) {
+      console.group('[COMMON] 확인용 로그');
+      console.log('확인 메시지 : ', message);
+      console.log('data : ', data);
+      console.groupEnd();
+    }
+  },
+
+  error(message: string, error: AxiosError) {
+    if (isDevelop()) {
+      console.group('[ERROR] API Error:');
+      console.log('status : ', error.response?.status);
+      console.log('Error : ', error.message);
+      console.log('response : ', error.response?.data);
+      console.groupEnd();
+    }
+  },
+
+  request(config: AxiosRequestConfig) {
+    if (isDevelop()) {
+      console.group('[API REQUEST] request: ');
+      console.log('config: ', {
+        method: config.method,
+        url: config.url,
+        headers: config.headers,
+        data: config.data,
+      });
+      console.groupEnd();
+    }
+  },
+
+  response(response: AxiosResponse) {
+    if (isDevelop()) {
+      console.group(
+        `✅ API Response: ${response.config.method?.toUpperCase()} ${response.config.url}`,
+      );
+      console.log('Status:', response.status);
+      console.log('Data:', response.data);
+      console.groupEnd();
+    }
+  },
+
+  performance(startTime: number, endTime: number, url: string) {
+    if (process.env.NODE_ENV === 'development') {
+      const duration = endTime - startTime;
+      console.log(`⏱️ API Performance: ${url} - ${duration.toFixed(2)}ms`);
+    }
+  },
+};
