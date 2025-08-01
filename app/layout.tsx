04fadccf739ff31596f1846382f28c1cfbdab84e
mxkxx <mxkxx@naver.com>
2025-08-01 16:51:11 +0900

    💄 chore: 컬러 및 폰트 세팅

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
-      <body className='antialiased'>
+      <body>
         <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
