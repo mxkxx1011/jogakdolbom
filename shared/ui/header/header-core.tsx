1758c952691cac51e4838f0f450780dd9c896c4a
mxkxx <mxkxx@naver.com>
2025-09-03 22:35:05 +0900

    style: 헤더에 반응형 디자인 추가

--- shared/ui/header/header-core.tsx
+++ shared/ui/header/header-core.tsx
@@ -8,9 +8,9 @@ interface Props {
 
 function HeaderCore({ left, center, right }: Props) {
   return (
-    <header className='fixed top-0 left-0 right-0 z-(--z-fixed) py-4 bg-white h-20 flex items-center'>
+    <header className='fixed top-0 left-0 right-0 z-(--z-fixed) py-4 bg-white h-20 flex items-center px-4 md:px-6 lg:px-8'>
       <section className='flex items-center justify-between w-300 mx-auto'>
-        <div className='w-32 h-8 relative'>{left}</div>
+        <div className='w-7 md:w-32 h-8 relative'>{left}</div>
         <div>{center}</div>
         <div>{right}</div>
       </section>
--- shared/ui/logo.tsx
+++ shared/ui/logo.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 function Logo() {
   return (
     <Link href='/' aria-label='로고-홈으로 이동'>
-      <Image src='/images/logo_width.png' alt='로고' fill priority />
+      <Image src='/images/logo_width.png' alt='로고' fill priority className='hidden md:block' />
+      <Image src='/images/logo_only.png' alt='로고' width={28} height={28} priority className='block md:hidden' />
     </Link>
   );
 }
