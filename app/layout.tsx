a812e98af1a36f6156f7e292183098b46e066adf
mxkxx1011 <mxkxx@naver.com>
2025-09-16 08:23:52 +0000

    [#64] feat: 메타데이터 설정
    * [#64] feat: 사이트 seo config 파일 추가 및 메타데이터 설정
    
    * [#64] feat: 구글 애널리틱스 스크립트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import Script from 'next/script';
 import { type ReactNode } from 'react';
 
 import { QueryProvider } from '@/shared/api';
-import { Toaster, Modal } from '@/shared/ui';
+import { SITE } from '@/shared/config/seo';
+import { GoogleAnalytics } from '@/shared/lib/google-analytics';
+import { Modal, Toaster } from '@/shared/ui';
 import { Header } from '@/widgets/header';
 
 import type { Metadata } from 'next';
@@ -10,8 +12,39 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: '조각돌봄',
-  description: '돌봄의 조각들을 이웃들과 나눠보세요',
+  metadataBase: new URL(SITE.domain),
+  title: {
+    default: `${SITE.name}`,
+    template: `%s | ${SITE.name}`,
+  },
+  description: SITE.description,
+  keywords: SITE.keywords,
+  alternates: {
+    canonical: SITE.domain,
+  },
+  openGraph: {
+    type: 'website',
+    url: SITE.domain,
+    title: SITE.name,
+    description: SITE.description,
+    siteName: SITE.name,
+    images: [
+      {
+        url: SITE.ogImage,
+        width: 1200,
+        height: 630,
+        alt: `${SITE.name} 미리보기`,
+      },
+    ],
+    locale: 'ko_KR',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: SITE.name,
+    description: SITE.description,
+    images: [SITE.ogImage],
+  },
+  icons: SITE.icons,
 };
 
 export default function RootLayout({
@@ -21,6 +54,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ko'>
+      <head>
+        <GoogleAnalytics />
+      </head>
       <body>
         <Script
           strategy='beforeInteractive'
---
+++ shared/config/seo.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+  name: '조각돌봄',
+  domain: 'https://jogakdolbom.vercel.app',
+  ogImage: '/images/og-image.png',
+  description: '돌봄의 조각들을 이웃들과 나눠보세요',
+  keywords: ['조각돌봄, 돌봄, 돌봄 공백, 틈새돌봄, 토큰, 조각'],
+  icons: {
+    icon: '/images/favicon.ico',
+  },
+};
